c9f0922f899cc9c92cd31ed1d63281a36c2895c7
Jorge Hernandez <jorge.hernandez@dreamcodesoft.com>
2021-02-19 23:36:39 -0500

    firsr versión

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { LoginComponent } from './pages/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { IndexComponent } from './pages/index/index.component';
+import { HistoryComponent } from './components/history/history.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MenuLoginComponent } from './components/menu-login/menu-login.component';
+//forms
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { GalleryComponent } from './components/gallery/gallery.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    CarouselComponent,
+    IndexComponent,
+    HistoryComponent,
+    LoginComponent,
+    MenuLoginComponent,
+    GalleryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
